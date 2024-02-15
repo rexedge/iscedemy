aeee5e0fc078eba382a3b89cf8791a6700a5baee
oyeniran Ayobami Paul <ap.oyeniran@gmail.com>
2024-02-15 16:40:15 +0100

    feat: created nav bar for mobile

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,75 +1,119 @@
 import React from 'react';
 import MaxWidthWrapper from './max-width-wrapper';
 import Link from 'next/link';
+import {
+	LucideApple,
+	LucideFacebook,
+	LucideInstagram,
+	LucideLinkedin,
+	LucideMail,
+	LucideMapPin,
+	LucidePhone,
+	LucideTwitter,
+	LucideYoutube,
+} from 'lucide-react';
+import { Button } from '../ui/button';
 
 export default function Footer() {
 	return (
 		<div>
 			<MaxWidthWrapper>
-				<div className=' flex justify-between'>
-						<div className=' w-[310px]  grid gap-4'>
-							<ol className='font-bold text-lg'> Downlaod ISCE Stores</ol>
-							<ol className=' font-medium'>ISCE Store is a dynamic and inclusive e-commerce platform that seamlessly connects sellers and buyers, fatering a vibrant maarketplace</ol>
-						<ol className='flex gap-3'>
-							<ol className='bg-black w-36 h-12 flex items-center'>
-								<ol><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="white" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-apple"><path d="M12 20.94c1.5 0 2.75 1.06 4 1.06 3 0 6-8 6-12.22A4.91 4.91 0 0 0 17 5c-2.22 0-4 1.44-5 2-1-.56-2.78-2-5-2a4.9 4.9 0 0 0-5 4.78C2 14 5 22 8 22c1.25 0 2.5-1.06 4-1.06Z"/><path d="M10 2c1 .5 2 2 2 5"/></svg></ol>
-								<ol>
-									<ol className='text-white text-[9px]'> Downlad On The</ol>
-									<ol className='text-white text-[16px] font-bold'> App Store </ol>
-								</ol>
+				<div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 justify-between'>
+					<div className='w-full flex flex-col gap-4'>
+						<div className='font-bold text-lg'>
+							Download ISCE Stores
+						</div>
+						<div className=' font-medium'>
+							ISCE Store is a dynamic and inclusive
+							e-commerce platform that seamlessly connects
+							sellers and buyers, fostering a vibrant
+							marketplace
+						</div>
+						<ol className='grid grid-cols-2 gap-4'>
+							<li className='bg-black h-12 justify-center gap-2 flex items-center text-white rounded-lg'>
+								<LucideApple />
+								<div>
+									<div className='text-white text-[9px]'>
+										Download On The
+									</div>
+									<div className='text-white text-[16px] font-bold'>
+										App Store
+									</div>
+								</div>
+							</li>
+							<li className='bg-black h-12 justify-center gap-2 flex items-center text-white rounded-lg'>
+								<LucideApple />
+								<div>
+									<div className='text-white text-[11px]'>
+										Get it In
+									</div>
+									<div className='text-white text-[16px] font-bold'>
+										Google Play
+									</div>
+								</div>
+							</li>
 						</ol>
-						<ol className='bg-black w-36 h-12 flex items-center'>
-							<ol><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="white" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-apple"><path d="M12 20.94c1.5 0 2.75 1.06 4 1.06 3 0 6-8 6-12.22A4.91 4.91 0 0 0 17 5c-2.22 0-4 1.44-5 2-1-.56-2.78-2-5-2a4.9 4.9 0 0 0-5 4.78C2 14 5 22 8 22c1.25 0 2.5-1.06 4-1.06Z"/><path d="M10 2c1 .5 2 2 2 5"/></svg></ol>
-								<ol>
-									<ol className='text-white text-[11px]'> Get it In </ol>
-									<ol className='text-white text-[16px] font-bold'> Google Play </ol>
-								</ol>
+					</div>
+
+					<div className='w-full flex flex-col gap-4'>
+						<ol className='font-bold text-lg'> Contact Us</ol>
+						<ol className='flex gap-2'>
+							<LucideMapPin className='shrink-0' />
+							<ol>
+								4th floor, Polystar building, Manuwa bus
+								stop, Lekki phase 1, Lagos, Nigeria
 							</ol>
 						</ol>
-						</div>
-						
-						<div className='w-[310px]  grid gap-8'>
-							<ol className='font-bold text-lg'> Contact Us</ol>
-							<ol className='flex'>
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin"><path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"/><circle cx="12" cy="10" r="3"/></svg>
-									<ol>  4th floor, Polystar building, Manuwa bus stop, Lekki phase 1, Lagos, Nigeria</ol>
-							</ol>
-							<ol className='flex'>
-							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail"><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>
-									<ol>  Customersupport@24mobilemech.com</ol>
-							</ol>
-							<ol className='flex'>
-							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-phone"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"/></svg>
-									<ol> 09013 447 0693</ol>
-							</ol>
-						</div>
+						<ol className='flex gap-2'>
+							<LucideMail className='shrink-0' />
+							<ol> Customersupport@24mobilemech.com</ol>
+						</ol>
+						<ol className='flex gap-2'>
+							<LucidePhone className='shrink-0' />
+							<ol> 09013 447 0693</ol>
+						</ol>
+					</div>
 
-						<div className='grid gap-7'>
-							<ol className='font-bold text-lg'> Quick Link</ol>
-							<Link href='/about-us'> About Us</Link>
-							<Link href='/about-us'> Terms Of Use</Link>
-							<Link href='/about-us'> Privacy & Policy</Link>
-							<Link href='/about-us'> Frequently Asked Question</Link>
-						</div>
+					<div className='flex flex-col gap-4'>
+						<ol className='font-bold text-lg'> Quick Link</ol>
+						<Link href='/about-us'> About Us</Link>
+						<Link href='/about-us'> Terms Of Use</Link>
+						<Link href='/about-us'> Privacy & Policy</Link>
+						<Link href='/about-us'>
+							{' '}
+							Frequently Asked Question
+						</Link>
+					</div>
 
-						<div className='grid gap-5 justify-center'>
-							<ol className='font-bold text-lg'> Subscribe To Our Newsletter</ol>
-							<input placeholder='Search' type = "text" className=' px-1 py-1 border-none bg-slate-400 text-black rounded-lg' /> 
-							<Link href='/courses' className=' font-bold text-[15px] items-center px-20 py-2 bg-black text-white rounded-lg h-10'>Subscribe</Link>
+					<div className='flex flex-col gap-4'>
+						<div className='font-bold text-lg'>
+							{' '}
+							Subscribe To Our Newsletter
 						</div>
-						
+						<div className='border-b-2 rounded-lg h-12'>
+							<input
+								placeholder='Email Address'
+								type='text'
+								className='px-3 w-full h-full border-none bg-slate-100  text-black rounded-lg placeholder:text-black'
+							/>
+						</div>
+						<Button className='h-12 rounded-2xl'>
+							Subscribe
+						</Button>
+					</div>
 				</div>
-				<div className=' pt-24 '>
-							<div className='flex items-center gap-4 justify-center'>
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill='none' stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-twitter"><path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"/></svg>
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg>
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-instagram"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-youtube"><path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17"/><path d="m10 15 5-3-5-3z"/></svg>
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
-							</div>					
-							<div className=' grid justify-center'> 2024 ISCE Stores. All Rights Reserved.</div>
+				<div className='pt-10 '>
+					<div className='flex items-center gap-4 justify-center'>
+						<LucideTwitter />
+						<LucideFacebook />
+						<LucideInstagram />
+						<LucideYoutube />
+						<LucideLinkedin />
+					</div>
+					<div className='grid justify-center pb-10'>
+						2024 ISCE Stores. All Rights Reserved.
+					</div>
 				</div>
-				
 			</MaxWidthWrapper>
 		</div>
 	);
